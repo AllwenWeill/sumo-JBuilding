b34d8f4b206b4fe4e2e803f717af33c63ea76001
AllwenWeill <1041268906@qq.com>
2023-03-21 09:06:08 -0700

    add class of three types of buildXML

---
+++ code/buildFile/buildXML_Row.h
@@ -0,0 +1,42 @@
+#ifndef BUILDXML_ROW_H
+#define BUILDXML_ROW_H
+#include "string"
+struct flowInformation{
+    std::string id; 
+    std::string type;
+    std::string color;
+    std::string begin;
+    std::string end;
+    std::string from;
+    std::string to;
+    std::string number;
+    std::string departLane;
+    std::string arrivalLane;
+};
+struct flowType{
+    std::string id;
+    std::string vClass;
+    std::string lcStrategic;
+    std::string carFollowingModel;
+    std::string accel;
+    std::string decel;
+    std::string emergencyDecel;
+    std::string minGap;
+    std::string tau;
+    std::string speedDev;
+    std::string sigma;
+    std::string actionStepLength;
+};
+
+class BuildXML_Row{
+private:
+    int m_carNum; //total number of cars
+    double m_penetrationRate;
+    int m_flowNum; //the number of car flow
+public:
+    BuildXML_Row();
+    ~BuildXML_Row();
+    void extractFeature_Row();
+};
+
+#endif
