489af5fc05e2827813ba71c20ebffbb08a816961
AllwenWeill <1041268906@qq.com>
2023-04-09 08:07:16 -0700

    add code for buildXML_Config

--- code/buildFile/buildXML_Config.h
+++ code/buildFile/buildXML_Config.h
@@ -1,12 +1,32 @@
+#pragma once
 #ifndef BUILDXML_CONFIG_H
 #define BUILDXML_CONFIG_H
-class BuildXML_Config{
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <filesystem>
+#include <fstream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+namespace fs = std::filesystem;
+class BuildXML_Config {
 private:
-
+    std::string m_content; //original text
+    fs::path configPath;
+    bool m_isTurnOnEmissions;
+    bool m_isTurnOnTripinfo;
+    double m_beginTime;
+    double m_endTime;
+    double m_stepTime;
 public:
     BuildXML_Config();
+    BuildXML_Config(std::string content);
     ~BuildXML_Config();
     void extractFeature_Config();
+    bool writeConfig(); //build executable xml file
 };
 
 #endif
