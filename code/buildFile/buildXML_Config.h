9b28fcda538eec0ab3ce66b50ccba068fd5fabfe
AllwenWeill <1041268906@qq.com>
2023-04-12 08:56:01 -0700

    update buildXML_Config class

--- code/buildFile/buildXML_Config.h
+++ code/buildFile/buildXML_Config.h
@@ -15,18 +15,23 @@ namespace fs = std::filesystem;
 class BuildXML_Config {
 private:
     std::string m_content; //original text
+    int m_curPtr; //pointer to the current character
     fs::path configPath;
     bool m_isTurnOnEmissions;
     bool m_isTurnOnTripinfo;
-    double m_beginTime;
-    double m_endTime;
-    double m_stepTime;
+    string m_beginTime;
+    string m_endTime;
+    string m_stepTime;
+    std::unordered_set<std::string> keyword_uset;
 public:
     BuildXML_Config();
     BuildXML_Config(std::string content);
     ~BuildXML_Config();
     void extractFeature_Config();
     bool writeConfig(); //build executable xml file
+    void init_keyword_uset();
+    void advance(); //the pointer forward a step
+    std::string getValue();
 };
 
 #endif
